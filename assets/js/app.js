719ee1d5b66d7fa9e3860eecc5112b6d020cd54b
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-20 15:57:54 +0200

    add style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
+/* 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus (fatelo):
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
 const app = new Vue ({
-    
+    el: '#app',
+    data: {
+        message: '"Forever blowing bubbles"',
+        image: './assets/img/hool_pic.jpeg',
+        team: ''
+    },
+
 })
